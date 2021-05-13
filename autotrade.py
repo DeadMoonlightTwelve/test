326c344ec7c7d7e475da1a11bbad1b44562b1b5d
DeadMoonlightTwelve <35948665+DeadMoonlightTwelve@users.noreply.github.com>
2021-05-13 23:41:00 +0900

    매매전략 개선: 조건 변경 및 관심 코인 설정

--- autotrade.py
+++ autotrade.py
@@ -5,43 +5,35 @@ import datetime
 access = ""
 secret = ""
 
-k = 0.4
-h = 1.2
-tickers = pyupbit.get_tickers(fiat="KRW")
+k = 0.6
+tickers = ["KRW-BTC", "KRW-ETH", "KRW-BCH", "KRW-LTC", "KRW-NEO", "KRW-BTG",
+           "KRW-ETC", "KRW-STRK", "KRW-LINK", "KRW-DOT", "KRW-WAVES", "KRW-FLOW"
+           "KRW-QTUM", "KRW-GAS", "KRW-EOS", "KRW-OMG", "KRW-TON", "KRW-CBK",
+           "KRW-DAWN", "KRW-ONT", "KRW-ADA", "KRW-XRP", "KRW-XLM", "KRW-HIVE", "KRW-DOGE"]
 bought_ticker = {}
 
 
 def get_target_price(ticker):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=2)
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+
     target_price = df.iloc[0]['close'] + \
         (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 
 def get_start_time(ticker):
-    """시작 시간 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="minute240", count=1)
     start_time = df.index[0]
     return start_time
 
 
-def get_volume_ma(ticker):
-    """5시간 거개량 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=6)
-    volume = df['volume'].rolling(5).mean().shift(1)
-    return volume[-1]
-
-
 def get_price_ma(ticker):
-    """10시간 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=11)
-    ma = df['close'].rolling(10).mean().shift(1)
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=6)
+    ma = df['close'].rolling(5).mean().shift(1)
     return ma[-1]
 
 
 def get_balance(ticker):
-    """잔고 조회"""
     balances = upbit.get_balances()
     for b in balances:
         if b['currency'] == ticker:
@@ -49,10 +41,10 @@ def get_balance(ticker):
                 return float(b['balance'])
             else:
                 return 0
+    return 0
 
 
 def get_avg_buy_price(ticker):
-    """ 매수 평균가 조회 """
     balances = upbit.get_balances()
     for b in balances:
         if b['currency'] == ticker:
@@ -60,17 +52,11 @@ def get_avg_buy_price(ticker):
                 return float(b['avg_buy_price'])
             else:
                 return 0
+    return 0
 
 
 def get_current_price(ticker):
-    """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
-
-
-def get_current_volume(ticker):
-    """현재 거래량 조회"""
-    one_hour_df = pyupbit.get_ohlcv(ticker, interval="minute60", count=1)
-    return one_hour_df['volume'][0]
 
 
 # 로그인
@@ -90,26 +76,26 @@ while True:
                     target_price = get_target_price(ticker)
                     price_ma = get_price_ma(ticker)
                     current_price = get_current_price(ticker)
-                    volume_ma = get_volume_ma(ticker)
-                    current_volume = get_current_volume(ticker)
 
-                    if target_price < current_price and price_ma < current_price and volume_ma * h < current_volume:
+                    if target_price < current_price and price_ma < current_price:
                         krw = get_balance("KRW")
                         if krw > 5000:
                             upbit.buy_market_order(ticker, krw*0.9995)
                             bought_ticker[ticker] = True
                 time.sleep(1)
         else:
+            ticker = list(bought_ticker.keys())[0]
             start_time = get_start_time(ticker)
             end_time = start_time + datetime.timedelta(hours=4)
             ticker_balance = get_balance(ticker[4:])
-            avr_buy_price = get_avg_buy_price(ticker)
+            avr_buy_price = get_avg_buy_price(ticker[4:])
+            current_price = get_current_price(ticker)
 
-            if now >= end_time - datetime.timedelta(seconds=30):
+            if now >= end_time:
                 if ticker_balance * avr_buy_price > 5000:
                     upbit.sell_market_order(ticker, ticker_balance*0.9995)
                     bought_ticker.pop(ticker, None)
-            if avr_buy_price * 1.04 < current_price or avr_buy_price * 0.98 > current_price:
+            if avr_buy_price * 1.07 < current_price or avr_buy_price * 0.97 > current_price:
                 if ticker_balance * avr_buy_price > 5000:
                     upbit.sell_market_order(ticker, ticker_balance*0.9995)
                     bought_ticker.pop(ticker, None)
